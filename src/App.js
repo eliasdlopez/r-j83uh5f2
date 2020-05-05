150037ec887c0f0c1d3037cecb58e231233b5c0d
Elias <eliasdamianlopez@gmail.com>
2020-05-04 20:36:11 -0500

    solucionado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count: 0
+  }
+
+  // handleChange = (event) => {
+  //   this.setState((esto SIEMPRE ES UN CALLBACK DE PREVSTATE) => ({
+  //     count: event.target.value.length
+  //   }));
+  // }
+
+  handleChange = (event) => {
+    this.setState({
+      count: event.target.value.length
+    });
+  };
+  
+  
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.handleChange}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
